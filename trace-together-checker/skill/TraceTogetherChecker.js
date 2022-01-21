1fe05a05f217a15a78748573f85eea74cfe2b56f
Sebastian <sebastianliando@gmail.com>
2022-01-21 18:04:04 +0800

    Add reconnection to server

--- trace-together-checker/skill/TraceTogetherChecker.js
+++ trace-together-checker/skill/TraceTogetherChecker.js
@@ -6,84 +6,62 @@ misty.RegisterUserEvent("OnUserCloseBy", true);
 misty.RunSkill("29e71806-c2f7-46f4-b185-976dd0da3b27");
 
 function _OnUserCloseBy({ closeBy, distance }) {
+  // Give instruction to the user.
   if (closeBy) {
     misty.Speak(
       "Hello, please put your phone on the table and then tap my head"
     );
-  } else {
-    // TBD
   }
 }
 
-// Try to connect to the server
-misty.SendExternalRequest(
-  "GET",
-  _params.baseUrl,
-  null,
-  null,
-  JSON.stringify({}),
-  "false",
-  "false",
-  "filename.png",
-  "application/json",
-  "_ServerCheck"
-);
+/**
+ * Send GET request to the server's home endpoint.
+ * The URL is specified in the parameter of the skill.
+ */
+function callHomeEndpoint() {
+  misty.SendExternalRequest(
+    "GET",
+    _params.baseUrl,
+    null,
+    null,
+    JSON.stringify({}),
+    "false",
+    "false",
+    "filename.png",
+    "application/json",
+    "_ServerCheck" // Name of the callback function
+  );
+}
+
+callHomeEndpoint();
 
 function _ServerCheck(data) {
   const status = data.Status;
 
   if (status === 3) {
+    // If successful -> setup the skill
     misty.Debug("Success!");
     misty.Speak("Connected to the Server", true);
     setupSkill();
   } else {
+    // If fail -> retry to call the home endpoint.
     misty.Debug("Error!");
     misty.Debug(data.ErrorMessage);
-    misty.Speak("Unable to connect to the server", true);
+    misty.Speak("Unable to connect to the server, retrying", true);
+    callHomeEndpoint();
   }
 }
 
 function setupSkill() {
-  // Add touch sensor for head
+  // Listen to touch sensor on Misty's head.
   misty.AddReturnProperty("OnTouch", "sensorPosition");
   misty.RegisterEvent("OnTouch", "TouchSensor", 1000, true);
-
-  waitUserSpeech();
-}
-
-function waitUserSpeech() {
-  // Wait until user says "Hey Misty, ...." then send the audio to the server
-  misty.AddPropertyTest("OnSpeech", "success", "==", "true", "boolean");
-  misty.AddReturnProperty("OnSpeech", "filename");
-  misty.RegisterEvent("OnSpeech", "VoiceRecord", 100, false);
-  misty.StartKeyPhraseRecognition(true);
-}
-
-function _OnSpeech(data) {
-  const filename = data.AdditionalResults;
-
-  misty.Debug(filename);
-  misty.GetAudioFile(filename);
-}
-
-function _GetAudioFile(data) {
-  if (data.Status === 3) {
-    const result = data.Result;
-    speechToText(result.Base64);
-  }
 }
 
 function _OnTouch(data) {
   const [sensorPos] = data.AdditionalResults;
 
-  misty.Debug(sensorPos);
-
-  if (sensorPos === "Chin") {
-    misty.Speak("How may I help you?", true);
-    waitUserSpeech();
-    return;
-  }
-
+  // Only respond to front head sensor events.
   if (sensorPos !== "HeadFront") {
     return;
   }
@@ -91,26 +69,34 @@ function _OnTouch(data) {
   checkTraceTogether();
 }
 
+/**
+ * Misty takes picture and sends it to the local server to validate TraceTogether certificate.
+ */
 function checkTraceTogether() {
+  // Give feedback to the user that Misty is taking picture.
   misty.DisplayImage("e_SystemCamera.jpg");
   misty.PlayAudio("s_SystemCameraShutter.wav", 10);
+
+  // Takes 4K picture. This will invoke the callback function _TakePicture().
   misty.TakePicture("trace-together", 4160, 3120, false, true);
 }
 
 function _TakePicture(data) {
+  // Reset Misty's facial expression.
   misty.DisplayImage("e_DefaultContent.jpg");
 
   if (data.Status === 3) {
+    // If successful, send the image to server.
     const result = data.Result;
-    misty.Debug("Successfully taken picture: " + result.ContentType);
-    checkTraceTogetherImage(result.Base64);
+    sendTraceTogetherImage(result.Base64);
   } else {
+    // Handle error if any.
     misty.Debug("Failed to take picture!");
     misty.Speak("Failed to take picture! Please try again.");
   }
 }
 
-function checkTraceTogetherImage(base64) {
+function sendTraceTogetherImage(base64) {
   misty.SendExternalRequest(
     "POST",
     _params.baseUrl + "/check",
@@ -126,8 +112,8 @@ function checkTraceTogetherImage(base64) {
 }
 
 function _TraceTogetherResult(data) {
+  // Parse the response.
   const response = JSON.parse(data.Result.ResponseObject.Data);
-
   misty.Debug(JSON.stringify(response));
 
   // Check for errors in detection
@@ -137,6 +123,7 @@ function _TraceTogetherResult(data) {
     return;
   }
 
+  // If no error, process the response
   handleTraceTogetherResult(response);
 }
 
@@ -147,9 +134,8 @@ function handleTraceTogetherResult({
   safeEntry,
   vaccinated,
 }) {
-  const isCheckInCert = (checkIn && safeEntry) || vaccinated;
-
   // Make sure it is a trace-together check-in certificate
+  const isCheckInCert = (checkIn && safeEntry) || vaccinated;
   if (!isCheckInCert) {
     misty.Speak("Sorry! Invalid trace together check-in certificate");
     return;
@@ -161,7 +147,7 @@ function handleTraceTogetherResult({
     return;
   }
 
-  // Date or location
+  // Check date and location
   if (dateValid && locationValid) {
     misty.Speak("Thank you! Welcome to NTU - N3 AND N4 CLUSTER");
   } else {
