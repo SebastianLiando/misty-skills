fe2e4c966e489f9a90f4ef9fde2ea85b5e163886
Sebastian <sebastianliando@gmail.com>
2022-02-24 23:21:03 +0800

    Add data layer for robot

---
+++ trace-together-checker/server/app/database/mongo.py
@@ -0,0 +1,67 @@
+import pymongo
+from bson.objectid import ObjectId
+
+# The name of the database.
+DB_NAME = 'misty_tracer'
+DEFAULT_CLIENT = pymongo.MongoClient(
+    host='localhost',
+    port=27017,
+    connect=False  # Lazy connect on the first operation
+)
+
+
+class MongoRepository():
+    def __init__(self, client: pymongo.MongoClient = DEFAULT_CLIENT, db_name: str = DB_NAME) -> None:
+        database = client.get_database(db_name)
+        self.collection = database[self.collection_name]
+
+    @property
+    def collection_name(self) -> str:
+        raise NotImplementedError()
+
+    def to_json(self, item):
+        raise NotImplementedError()
+
+    def from_json(self, json):
+        raise NotImplementedError()
+
+    def _parse_id(self, id) -> ObjectId:
+        if isinstance(id, ObjectId):
+            return id
+        else:
+            return ObjectId(id)
+
+    def list(self, query={}):
+        return [self.from_json(x) for x in self.collection.find(query)]
+
+    def get(self, id):
+        target_id = self._parse_id(id)
+        return self.from_json(self.collection.find_one({'_id': target_id}))
+
+    def save(self, item):
+        data: dict = self.to_json(item)
+
+        if '_id' not in data:
+            result = self.collection.insert_one(data)
+            updated_id = result.inserted_id
+        else:
+            updated_id = self._parse_id(data.pop('_id'))
+            self.collection.update_one({'_id': updated_id},
+                                       update={"$set": data},
+                                       )
+
+        return self.get(updated_id)
+
+    def delete(self, id):
+        target_id = self._parse_id(id)
+        return self.from_json(self.collection.find_one_and_delete(target_id))
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(
+                Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
---
+++ trace-together-checker/server/app/database/robot.py
@@ -0,0 +1,107 @@
+from dataclasses import dataclass
+from enum import Enum
+from datetime import datetime
+from typing import Optional
+from bson.objectid import ObjectId
+from .mongo import MongoRepository, DEFAULT_CLIENT, DB_NAME, Singleton
+from pymongo import MongoClient, ASCENDING
+
+
+class RobotState(Enum):
+    OFFLINE = 'OFFLINE'
+    """Robot is offline."""
+    PENDING = 'PENDING'
+    """Robot needs to be assigned a location."""
+    IDLE = 'IDLE'
+    """Robot is idle."""
+    ENGAGING = 'ENGAGING'
+    """Robot detects nearby user and is detecting cell phone."""
+    CAPTURING = 'CAPTURING'
+    """Robot is taking a picture."""
+    VERIFYING = 'VERIFYING'
+    """Robot is verifying TraceTogether check-in certificate."""
+    REJECT = 'REJECT'
+    """Robot rejects the user's TraceTogether certificate."""
+    ACCEPT = 'ACCEPT'
+    """Robot accepts the user's TraceTogether certificate."""
+
+
+@dataclass
+class Robot:
+    id: str
+    serial: str
+    location: str
+    current_state: RobotState
+    state_updated_at: datetime
+
+    def to_json(self) -> dict:
+        json = {
+            'serial': self.serial,
+            'location': self.location,
+            'current_state': self.current_state.value,
+            'state_updated_at': self.state_updated_at
+        }
+
+        if self.id != '':
+            json['_id'] = self.id
+
+        return json
+
+    @staticmethod
+    def from_json(json: dict):
+        return Robot(
+            id=str(json['_id']),
+            serial=json['serial'],
+            location=json['location'],
+            current_state=RobotState(json['current_state']),
+            state_updated_at=json['state_updated_at']
+        )
+
+
+class RobotRepository(MongoRepository, metaclass=Singleton):
+    def __init__(self, client: MongoClient = DEFAULT_CLIENT, db_name: str = DB_NAME) -> None:
+        super().__init__(client, db_name)
+
+        # Create unique index on the serial number.
+        self.collection.create_index(
+            [("serial", ASCENDING), ],
+            unique=True,
+        )
+
+    @property
+    def collection_name(self) -> str:
+        return "robots"
+
+    def to_json(self, item: Robot):
+        return item.to_json()
+
+    def from_json(self, json):
+        return Robot.from_json(json)
+
+    def get_by_serial(self, serial: str) -> Optional[Robot]:
+        doc = self.collection.find_one({'serial': serial})
+        if doc is None:
+            return None
+
+        return self.from_json(doc)
+
+    def register(self, serial: str) -> Robot:
+        new_robot = Robot(
+            id='',
+            serial=serial,
+            location='',
+            # When a new robot is registered, it is in pending state
+            # waiting for an administrator to assign location.
+            current_state=RobotState.PENDING,
+            state_updated_at=datetime.utcnow()
+        )
+
+        return self.save(new_robot)
+
+    def update_state(self, serial: str, state: RobotState) -> Robot:
+        robot = self.get_by_serial(serial)
+
+        robot.current_state = state
+        robot.state_updated_at = datetime.utcnow()
+
+        return self.save(robot)
