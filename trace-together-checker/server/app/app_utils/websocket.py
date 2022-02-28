7a7c5da3ce32df0fe2ef55ff3bf525e1f1d8b002
Sebastian <sebastianliando@gmail.com>
2022-02-28 11:03:54 +0800

    Add publish subscribe

--- trace-together-checker/server/app/app_utils/websocket.py
+++ trace-together-checker/server/app/app_utils/websocket.py
@@ -1,27 +1,69 @@
-from typing import List
+from datetime import datetime
+from typing import Set
 from fastapi import WebSocket
 import json
 from dependencies import Singleton
 
+from enum import Enum
+from collections import defaultdict
+
+
+class MessageTypes(Enum):
+    SUBSCRIBE = 'SUBSCRIBE'
+    UNSUBSCRIBE = 'UNSUBSCRIBE'
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+
+        return super().default(o)
+
 
 class WSConnectionManager(metaclass=Singleton):
     def __init__(self):
-        self.active_connections: List[WebSocket] = []
+        self.active_connections: Set[WebSocket] = set()
+
+        self.subscriptions = defaultdict(lambda: set())
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections.add(websocket)
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+        # Remove from subscriptions
+        for topic in MessageTypes:
+            self.subscriptions[topic].discard(topic)
+
+    def subscribe(self, topic: str, websocket: WebSocket):
+        self.subscriptions[topic].add(websocket)
+        print(f'Client subscribed to {topic}')
+
+    def unsubscribe(self, topic: str, websocket: WebSocket):
+        self.subscriptions[topic].remove(websocket)
+        print(f'Client unsubscribed from {topic}')
+
+    def _json_to_str(self, body) -> str:
+        return json.dumps(body, cls=DateTimeEncoder)
+
+    async def publish(self, topic: str, data):
+        message = self._json_to_str(data)
+        for subscriber in self.subscriptions[topic]:
+            await self.send_personal_message(message, subscriber)
+
+    async def send_personal_message(self, message, websocket: WebSocket):
+        body = message if isinstance(message, str) else \
+            self._json_to_str(message)
+
+        await websocket.send_text(body)
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
 
     async def broadcast_json(self, body: dict):
-        json_str = json.dumps(body)
+        json_str = self._json_to_str(body)
         await self.broadcast(json_str)
--- trace-together-checker/server/app/routers/websocket.py
+++ trace-together-checker/server/app/routers/websocket.py
@@ -1,8 +1,39 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
-from app_utils.websocket import WSConnectionManager
+from app_utils.websocket import WSConnectionManager, MessageTypes
+from database.robot import RobotRepository
+from database.verification import VerificationRepository
+import json
 
 manager = WSConnectionManager()
 router = APIRouter(prefix='/ws', tags=['WebSocket'])
+
+TOPIC_ROBOT = 'ROBOT'
+TOPIC_VERIFICATION = 'VERIFICATION'
+
+async def process_topic_data(client: WebSocket, message_type: str, verifications: dict):
+    try:
+        parsed = MessageTypes(message_type.upper())
+    except Exception:
+        return
+
+    if parsed == MessageTypes.SUBSCRIBE:
+        # Add to subscriber list
+        topic = verifications['topic'].upper()
+        manager.subscribe(topic, client)
+
+        # Publish the initial data
+        if topic == TOPIC_ROBOT:
+            repo = RobotRepository()
+            robots = repo.list()
+            robots = list(map(lambda r: r.to_json(), robots))
+            await manager.send_personal_message(robots, client)
+        elif topic == TOPIC_VERIFICATION:
+            repo = VerificationRepository()
+            verifications = repo.list()
+            await manager.send_personal_message(verifications, client)
+    elif parsed == MessageTypes.UNSUBSCRIBE:
+        topic = verifications['topic'].upper()
+        manager.unsubscribe(topic, client)
 
 
 @router.websocket('/')
@@ -12,7 +43,14 @@ async def connect_websocket(socket: WebSocket):
 
     try:
         while True:
-            await socket.receive_text()
+            message = await socket.receive_text()
+            body = json.loads(message)
+
+            topic = body['type']
+            data = body['data']
+
+            await process_topic_data(socket, topic, data)
+
     except WebSocketDisconnect:
         manager.disconnect(socket)
         print('Client disconnected.')
